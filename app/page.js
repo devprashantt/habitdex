53cfbb97d8c411f7953ce53eadc5e0d8f6b995b2
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-03-13 00:43:06 +0530

    minor changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import styles from "./page.module.scss";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
     <main className={styles.main}>
+      <div>
       <h1 className={styles.title}>Habit-Dex</h1>
+      {
+        (!user)?<><Link href="/sign-in"><button>Sign In</button></Link><Link href="/sign-up"><button>Sign Up</button></Link></>:<>
+        <Link href="/home"><button>Home</button></Link>
+        </>
+      }
+      </div>
     </main>
   );
 }
