e44835c47b99aefc986601353f24395e9328d3b5
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-03-14 02:17:31 +0530

    syncs new user to DB

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,11 +1,12 @@
+import RegisterUser from '@/components/RegisterUser';
 import { UserButton, currentUser } from '@clerk/nextjs'
-import React from 'react'
+import axios from 'axios';
 
 const page = async () => {
   const user = await currentUser();
-  
   return (
     <div>
+      <RegisterUser />
       <h1>hi {user?.firstName}</h1>
       <UserButton afterSignOutUrl="/"></UserButton>
     </div>
