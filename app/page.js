5dde5d38b91371fba615ab0b7ef8544d4e4e7f5a
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-14 23:26:24 +0530

    1st page done

--- app/page.js
+++ app/page.js
@@ -1,15 +1,30 @@
 import Link from "next/link";
-import styles from "./page.module.scss";
+import styles from "./globals.scss";
 import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const user = await currentUser();
   return (
     <main className={styles.main}>
+      <div id="first">
+        <div id="first-1">
+          <h1>Welcome to our Habit Tracking App</h1>
+          <h6>Track your habit,achieve your goals,live your best life</h6>
+          <Link href="/sign-up"><button>Get Started</button></Link>
+        </div>
+        <div id="first-2">
+
+        </div>
+      </div>
+      <div id="second">
+        <div>
+          <h1>Sign In</h1>
+        </div>
+      </div>
       <div>
-      <h1 className={styles.title}>Habit-Dex</h1>
       {
-        (!user)?<><Link href="/sign-in"><button>Sign In</button></Link><Link href="/sign-up"><button>Sign Up</button></Link></>:<>
+        (!user)?<><Link href="/sign-in"><button>Sign In</button></Link>
+        <Link href="/sign-up"><button>Sign Up</button></Link></>:<>
         <Link href="/home"><button>Home</button></Link>
         </>
       }
