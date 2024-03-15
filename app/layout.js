4f2ba0d7f2fc70b69193bbab0ff7ef99c0c1b2f4
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-15 12:24:12 +0530

    adds gilroy cdn

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,25 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from "next/font/google";
 import "./globals.scss";
-
-const inter = Inter({ subsets: ["latin"] });
+import myFont from '@/lib/font';
+import localFont from "next/font/local"
+import Head from 'next/head';
 
 export const metadata = {
   title: "Habit-Dex",
   description: "Track your habits and get motivated.",
 };
 
+
 export default function RootLayout({ children }) {
+
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <Head>
+          <link rel="stylesheet" href="https://gistcdn.githack.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css" />
+
+        </Head>
+        <body >{children}</body>
       </html>
     </ClerkProvider>
   );
