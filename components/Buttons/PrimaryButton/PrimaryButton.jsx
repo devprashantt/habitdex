72b4a6fedf3ab8405187c99caff9ebc55fd5cbd6
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-03-17 22:38:28 +0530

    adds page styles

--- components/Buttons/PrimaryButton/PrimaryButton.jsx
+++ components/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -1,7 +1,11 @@
+import styles from "./PrimaryButton.module.scss"
+
 const PrimaryButton = ({ children, ...props }) => {
 	return (
 		<div>
-			<button {...props}>{children}</button>
+			<button className={styles.button} {...props}>{children}</button>
 		</div>
 	);
 };
+
+export default PrimaryButton;
