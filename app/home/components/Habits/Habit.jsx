012ef0c16d5289defdbe66301768d71760c2d720
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-03-19 20:07:15 +0530

    fetch habits

--- app/home/components/Habits/Habit.jsx
+++ app/home/components/Habits/Habit.jsx
@@ -1,6 +1,24 @@
-const Habit = (props) => {
+import { habitFormIconsJson, themeColorJson } from "@/constants";
+import styles from "@/app/home/home.module.scss"
 
-  
+const Habit = (props) => {
+  const color = themeColorJson[props.color];
+
+  return (
+    <div className={styles.main__container__wrapper}
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <div className={styles.main__container}>
+        <div className={styles.main__container__child1}>
+          {habitFormIconsJson[props.icon]}
+          <p>{props.name}</p>
+        </div>
+        <p>{props.contributions_per_day}</p>
+      </div>
+    </div>
+  )
 
   return <div>Habit {props.count}</div>;
 };
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
-import habitFormIcons from "./icons";
+import {habitFormIcons, habitFormIconsJson} from "./icons";
 import { themeColors, themeColorJson } from "./colors";
 
 const resultPerPage = 5
 
-export { habitFormIcons, themeColors, themeColorJson, resultPerPage };
+export { habitFormIcons, habitFormIconsJson, themeColors, themeColorJson, resultPerPage };
