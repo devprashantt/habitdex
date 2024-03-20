43d86919127adc2254aa6253c80bdeb7fac4a090
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-03-21 01:00:12 +0530

    .

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,11 +1,12 @@
 import RegisterUser from "@/app/home/components/RegisterUser/RegisterUser";
 import { UserButton, currentUser } from "@clerk/nextjs";
 import HabitControls from "./components/Habits/HabitControls";
+import styles from "./home.module.scss";
 
 const page = async () => {
   const user = await currentUser();
   return (
-    <div>
+    <div className={styles.page__wrapper}>
       <RegisterUser />
       <h2>{user?.firstName}</h2>
       <UserButton afterSignOutUrl="/"></UserButton>
